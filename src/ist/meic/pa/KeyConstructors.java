73aee3f1ff74837eaf36d04360bf950d0554ccad
miguelcruz <miguelsantoscruz95@gmail.com>
2017-03-15 10:39:02 +0000

    ..

---
+++ src/ist/meic/pa/KeyConstructors.java
@@ -0,0 +1,11 @@
+/**
+ * Created by miguelcruz on 15-03-2017.
+ */
+package ist.meic.pa;
+
+public class KeyConstructors {
+
+    public static void main (String[] args){
+
+    }
+}
--- src/ist/meic/pa/Main.java
+++
@@ -1,8 +0,0 @@
-package ist.meic.pa;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
