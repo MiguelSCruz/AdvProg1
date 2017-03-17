928851d4b9d62d311150c7c75e155cc0160519a5
miguelcruz <miguelsantoscruz95@gmail.com>
2017-03-17 18:08:10 +0000

    Added annotations to the annotation class

--- src/ist/meic/pa/KeywordArgs.java
+++ src/ist/meic/pa/KeywordArgs.java
@@ -1,8 +1,13 @@
 package ist.meic.pa;
 
+import java.lang.annotation.*;
+
 /**
  * Created by miguelcruz on 17-03-2017.
  */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface KeywordArgs {
     String value();
 }
