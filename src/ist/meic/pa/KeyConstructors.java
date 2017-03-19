8992636797023c48b2f5d700f5bd3e3c4d6a3d8d
miguelcruz <miguelsantoscruz95@gmail.com>
2017-03-19 20:03:06 +0000

    Started using Javassit./nDeclared method to handle the variables assignment

--- src/ist/meic/pa/KeyConstructors.java
+++ src/ist/meic/pa/KeyConstructors.java
@@ -3,9 +3,31 @@
  */
 package ist.meic.pa;
 
+import javassist.*;
+
 public class KeyConstructors {
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws NotFoundException, ClassNotFoundException{
+        if (args.length != 1){
+            System.err.println("Usage: java ist.meic.pa.KeyConstructors <Class>");
+            System.exit(1);
+        }
+
+        ClassPool classPool = ClassPool.getDefault();
+        CtClass ctClass = classPool.get(args[0]);
+        CtConstructor[] ctConstructors = ctClass.getConstructors();
+        for (CtConstructor ctConstructor: ctConstructors){
+            if (ctConstructor.hasAnnotation(KeywordArgs.class)){
+                assigner(ctConstructor);
+            }
+        }
 
     }
+
+    public static void assigner(CtConstructor ctConstructor) throws ClassNotFoundException{
+        KeywordArgs annotation = (KeywordArgs) ctConstructor.getAnnotation(KeywordArgs.class);
+        String value = annotation.value();
+
+    }
+
 }
